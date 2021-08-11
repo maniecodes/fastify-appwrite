406aa238cc93257040c9668a075ca7f0bbcca46a
manasseh liassidji <manassehl9@gmail.com>
2021-08-12 00:09:21 +0100

    fix(index): Bug fix

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 "use strict";
 
 const fp = require("fastify-plugin");
-const { getUsers, postUsers } = require("./lib/services/user");
 const Client = require("./lib/client");
 const Users = require("./lib/services/users");
-
-const getUserOpts = {
-  schema: {
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          id: { type: "string" },
-          name: { type: "string" },
-        },
-      },
-    },
-  },
-  handler: (req, reply) => {
-    reply.send("decorated");
-  },
-};
 
 function plugin(fastify, opts, done) {
   const client = new Client();
@@ -28,8 +10,6 @@ function plugin(fastify, opts, done) {
   try {
     fastify.decorate("client", client);
     fastify.decorate("user", user);
-    fastify.decorate("getUser", getUsers);
-    fastify.decorate("postUser", postUsers);
 
     done();
   } catch (error) {
