d96d4242a10568688465e95705f32c70d6d0626a
manasseh liassidji <manassehl9@gmail.com>
2021-08-14 14:59:49 +0100

    feat(Database): Add database endpoints

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 
 const fp = require("fastify-plugin");
 const Client = require("./lib/client");
-const Account = require("./lib/services/account");
 const Users = require("./lib/services/users");
+const Account = require("./lib/services/account");
+const Database = require("./lib/services/database");
 
 function plugin(fastify, opts, done) {
   const client = new Client();
   const user = new Users(client);
   const account = new Account(client);
+  const database = new Database(client);
   try {
     fastify.decorate("client", client);
     fastify.decorate("user", user);
     fastify.decorate("account", account);
+    fastify.decorate("database", database);
 
     done();
   } catch (error) {
