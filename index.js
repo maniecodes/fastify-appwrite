c9253b08ef26e8fca8aa6aeb3bc241607077c5e4
manasseh liassidji <manassehl9@gmail.com>
2021-08-14 14:10:00 +0100

    feat(Account): Add account endpoints

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 const fp = require("fastify-plugin");
 const Client = require("./lib/client");
+const Account = require("./lib/services/account");
 const Users = require("./lib/services/users");
 
 function plugin(fastify, opts, done) {
   const client = new Client();
   const user = new Users(client);
+  const account = new Account(client);
   try {
     fastify.decorate("client", client);
     fastify.decorate("user", user);
+    fastify.decorate("account", account);
 
     done();
   } catch (error) {
--- test/index.test.js
+++ test/index.test.js
@@ -39,7 +39,7 @@ test("testing user already exist", async (t) => {
       "85dfd135face9124586ac3ece5b5ed6452f304f95a0f24e3022a57d0818d4ae5c171dd85e367ab32c606344b2b927e31cf596b59d069ae9b34120c36642d7a0df6730e3b82638d7967586ee2a8ed59f9db67dc4ec3e64d3067f6e9000799275f3bdbdb65d5bf0b54ddd31facaeb77bea62d0a5974a647587bbcc785e7f81935f"
     );
 
-  res = await app.user.create("nuth@gmail.com", "Zamanie93", "manasseh");
+  res = await app.user.create("nuth@gmail.com", "", "manasseh");
 
   t.same(res.status, 409);
 });
@@ -61,7 +61,10 @@ test("Create a new user", async (t) => {
       "85dfd135face9124586ac3ece5b5ed6452f304f95a0f24e3022a57d0818d4ae5c171dd85e367ab32c606344b2b927e31cf596b59d069ae9b34120c36642d7a0df6730e3b82638d7967586ee2a8ed59f9db67dc4ec3e64d3067f6e9000799275f3bdbdb65d5bf0b54ddd31facaeb77bea62d0a5974a647587bbcc785e7f81935f"
     );
 
-  res = await app.user.create("nutdh@gmail.com", "Zamanie93", "manasseh");
+  res = await app.user.create("nutdh@gmail.com", "", "manasseh");
+  if(res.status == 201){
+
+  }
 
   t.same(res.status, 201);
 });
@@ -272,3 +275,4 @@ test("Update user verification", async (t) => {
 
   t.same(res.status, 200);
 });
+
